88dd2c4dc7492dca016129fad0f7bc82c4c46c95
Top <topdev707@gmail.com>
2022-03-26 15:00:26 +0100

    update Generator component and style

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -1,38 +1,78 @@
-import React from "react";
-import { memeData } from "../memeData";
-
+import React, { useState, useEffect } from "react";
+import "../index.css";
 const Generator = () => {
-  const [memeImages, setMemeImages] = React.useState(memeData);
+  const [meme, setMeme] = useState({
+    upperText: "",
+    lowerText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setAllMemes] = useState([]);
 
   function getMemeImage() {
-    const memeArray = memeImages.data.memes;
-    const randomIndex = Math.floor(Math.random() * memeArray.length);
-    const url = memeArray[randomIndex].url;
-    setMemeImages((prevMeme) => ({
+    const memesArray = allMemes.data.memes;
+    const randomIndex = Math.floor(Math.random() * memesArray.length);
+    const link = memesArray[randomIndex].url;
+
+    setMeme((prevMeme) => ({
       ...prevMeme,
-      randomMeme: url,
+      randomImage: link,
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    getMemes();
+  }, []);
+
+  const getMemes = async () => {
+    const response = await fetch("https://api.imgflip.com/get_memes");
+    const data = await response.json();
+    setAllMemes(data);
+  };
+
   return (
-    <div className="form">
-      <div className="input-fields">
-        <input
-          type="text"
-          name="uppertext"
-          id="uppertext"
-          placeholder="Upper Text"
-        />
-        <input
-          type="text"
-          name="lowertext"
-          id="lowertext"
-          placeholder="Lower Text"
-        />
+    <main>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-fields">
+          <input
+            type="text"
+            name="upperText"
+            className="form-input"
+            placeholder="Upper Text"
+            value={meme.upperText}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="lowerText"
+            className="form-input"
+            placeholder="Lower Text"
+            value={meme.lowerText}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={getMemeImage}>Get a new meme image 🖼 </button>
+      </form>
+
+      <div className="meme-container">
+        <img src={meme.randomMeme} className="displayed-meme" alt="" />
+        <h2 className="meme-text top">{meme.upperText}</h2>
+        <h2 className="meme-text bottom">{meme.lowerText}</h2>
       </div>
-      <button onClick={getMemeImage}>Get a new meme image 🖼 </button>
-      <img src={memeImages.randomMeme} className="displayed-meme" alt="" />
-    </div>
+    </main>
   );
 };
 
